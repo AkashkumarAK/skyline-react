86605e2aebc64ffd72129c5cf17e2280388ffc1e
AkashkumarAK <01fe20bcs300@kletech.ac.in>
2022-11-27 21:21:01 +0530

    removed one page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from './components/cards';
+
 import Header from './components/Header';
 import Main from './components/main';
 import Footer from './components/footer';
